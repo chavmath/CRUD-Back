01621027e7d97361dc8d112b06d0323fe8476147
Matheo Chavez <chavmath@gmail.com>
2024-05-11 15:28:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const routes = require('./routes/routes.js');
 const app = express();
 const port = 3000;
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
 app.use('/', routes);
 
 app.listen(port, () => {
