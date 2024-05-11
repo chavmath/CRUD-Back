0561b11815b6e320930a693821ef82d51d788ab2
Matheo Chavez <chavmath@gmail.com>
2024-05-11 16:39:22 -0500

    Tickets

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,4 +1,6 @@
 const models = require('../models/models.js');
+const { db } = require('../firebase');
+const { collection, getDocs, addDoc, doc, updateDoc } = require('firebase/firestore');
 
 exports.getInmuebles = (req, res) => {
     res.json(models.inmuebles);
@@ -10,4 +12,12 @@ exports.getCopropietarios = (req, res) => {
 
 exports.getUnidades = (req, res) => {
     res.json(models.unidades[req.params.id] || {});
+};
+
+exports.createTicket = async (req, res) => {
+    const newTicket = req.body;
+    const ticketsCollection = collection(db, 'tickets');
+    const docRef = await addDoc(ticketsCollection, newTicket);
+    newTicket.id = docRef.id;
+    res.json(newTicket);
 };
--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,6 @@ const controllers = require('../controllers/controllers.js');
 router.get('/listainmuebles', controllers.getInmuebles);
 router.get('/listacopropietarios/:id', controllers.getCopropietarios);
 router.get('/listaunidades/:id', controllers.getUnidades);
+router.post('/tickets', controllers.createTicket);
 
 module.exports = router;
