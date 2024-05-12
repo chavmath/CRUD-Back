bf2dab053e3a9ee2600655cf04e8620036c460ac
Matheo Chavez <chavmath@gmail.com>
2024-05-11 19:32:36 -0500

    proxy

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const routes = require('./routes/routes.js');
+const httpProxy = require('http-proxy');
 const app = express();
 const port = 3000;
 
-// Usa cors como middleware
-app.use(cors({
-    origin: '*'
-}));
+// Configurar el proxy
+const proxy = httpProxy.createProxyServer();
+
+// Usa cors como middleware permitiendo solicitudes desde cualquier origen
+app.use(cors());
+
+// Ruta para redirigir las solicitudes a la API
+app.all('/tickets', (req, res) => {
+  proxy.web(req, res, { target: 'https://crud-back-mlk9.onrender.com' });
+});
 
 app.use('/', routes);
 
